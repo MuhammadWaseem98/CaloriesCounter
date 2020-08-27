74d9af32929c968c8da342029cb17ee5b26d16f2
Shahzaib Ali <Contact.ShahzaibAli@gmail.com>
2020-08-27 16:49:08 +0500

    Initial commit

---
+++ app/src/main/java/com/calories/counter/daily/tracker/MainActivity.java
@@ -0,0 +1,41 @@
+package com.calories.counter.daily.tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
